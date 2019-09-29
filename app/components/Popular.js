a614815e83daf2fb0f4a8640080a45f8e4cfbe44
robertxluo <robertxluo@gmail.com>
2019-09-29 15:29:22 -0500

    Change state of language on click

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
     return (
-      <ul className="flex-center">
+      <ul className="navbar flex-center">
         {languages.map(language => (
           <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
+            <button
+              className="btn-clear nav-link"
+              style={
+                language === this.state.selectedLanguage ? { color: 'rgb(187, 46, 31)' } : null
+              }
+              onClick={() => this.updateLanguage(language)}
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
