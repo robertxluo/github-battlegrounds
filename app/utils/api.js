7cf4aee39740a6239604678bb8fc4c8c2aba42ac
robertxluo <robertxluo@gmail.com>
2019-09-30 00:38:48 -0500

    Add battle result algorithm

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,3 +1,60 @@
+function getErrorMessage(message, username) {
+  if (message === 'Not Found') {
+    return `${username} doesn't exist`;
+  }
+
+  return message;
+}
+
+function getProfile(username) {
+  return fetch(`https://api.github.com/users/${username}`)
+    .then(res => res.json())
+    .then(profile => {
+      if (profile.message) {
+        throw new Error(getErrorMessage(profile.message, username));
+      }
+
+      return profile;
+    });
+}
+
+function getRepos(username) {
+  return fetch(`https://api.github.com/users/${username}/repos`)
+    .then(res => res.json())
+    .then(repos => {
+      if (repos.message) {
+        throw new Error(getErrorMessage(repos.message, username));
+      }
+
+      return repos;
+    });
+}
+
+function getStarCount(repos) {
+  return repos.reduce((count, { stargazers_count }) => count + stargazers_count, 0);
+}
+
+function calculateScore(followers, repos) {
+  return followers * 3 + getStarCount(repos) + repos.length;
+}
+
+function getUserData(player) {
+  return Promise.all([getProfile(player), getRepos(player)]).then(([profile, repos]) => ({
+    profile,
+    score: calculateScore(profile.followers, repos)
+  }));
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+export function battle(players) {
+  return Promise.all([getUserData(players[0]), getUserData(players[1])]).then(results =>
+    sortPlayers(results)
+  );
+}
+
 export function fetchPopularRepos(language) {
   const endpoint = window.encodeURI(
     `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
