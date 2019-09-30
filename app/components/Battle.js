eabd7b5a2c2dc95e65af08de4f8ea461841394d2
robertxluo <robertxluo@gmail.com>
2019-09-29 20:42:18 -0500

    Style PlayerInput component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
 
 function Instructions() {
@@ -23,11 +24,68 @@ function Instructions() {
   );
 }
 
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.username);
+  }
+
+  handleChange(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="center-text header-lg">Players</h1>
+        <form className="column player" onSubmit={this.handleSubmit}>
+          <label htmlFor="username" className="player-label">
+            {this.props.label}
+          </label>
+          <div className="row player-inputs">
+            <input
+              type="text"
+              id="username"
+              className="input-light"
+              placeholder="github username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            <button className="btn btn-dark" type="submit" disable={!this.state.username}>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+};
+
 export default class Battle extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Instructions />
+        <PlayerInput label="Player One:" onSubmit={value => console.log(value)} />
       </React.Fragment>
     );
   }
