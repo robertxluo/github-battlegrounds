c390d30423469f1f2c128331b841330926df978f
robertxluo <robertxluo@gmail.com>
2019-09-30 23:30:51 -0500

    Add results UI

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -36,7 +36,7 @@ function ReposGrid({ repos }) {
         const { login, avatar_url } = owner;
 
         return (
-          <li key={html_url} className="repo bg-light">
+          <li key={html_url} className="card bg-light">
             <h4 className="header-lg center-text">#{index + 1}</h4>
             <img className="avatar" src={avatar_url} alt={`Avatar for ${login}`} />
             <h2 className="center-text">
@@ -131,7 +131,7 @@ export default class Popular extends React.Component {
         <LanguagesNav selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />
 
         {this.isLoading() && <p>LOADING</p>}
-        {error && <p>{error}</p>}
+        {error && <p className="center-text error">{error}</p>}
         {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
       </React.Fragment>
     );
