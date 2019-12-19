e0ef04401618baefb35ebfa21a110f2726c2b00f
Robert Luo <robertxluo@gmail.com>
2019-12-18 22:10:57 -0600

    Implement code splitting feature

--- app/index.js
+++ app/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { ThemeProvider } from './contexts/theme';
-
-import Popular from './components/Popular';
-import Battle from './components/Battle';
-import Nav from './components/Nav';
-import Results from './components/Results';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Nav from './components/Nav';
+import Loading from './components/Loading';
+
+const Popular = React.lazy(() => import('./components/Popular'));
+const Battle = React.lazy(() => import('./components/Battle'));
+const Results = React.lazy(() => import('./components/Results'));
 
 class App extends React.Component {
   state = {
@@ -26,12 +28,15 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className="container">
               <Nav />
-              <Switch>
-                <Route exact path="/" component={Popular} />
-                <Route exact path="/battle" component={Battle} />
-                <Route path="/battle/results" component={Results} />
-                <Route render={() => <h1>404</h1>} />
-              </Switch>
+
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path="/" component={Popular} />
+                  <Route exact path="/battle" component={Battle} />
+                  <Route path="/battle/results" component={Results} />
+                  <Route render={() => <h1>404</h1>} />
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
