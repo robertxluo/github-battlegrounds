2418c3db59dc504bff648ca577dd090140ee1924
Robert Luo <robertxluo@gmail.com>
2019-12-18 21:02:29 -0600

    Add Navbar with routes

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { ThemeConsumer } from '../contexts/theme';
+import { NavLink } from 'react-router-dom';
+
+const activeStyle = {
+  color: '#d50000'
+};
 
 export default function Nav() {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className="row space-between">
-          <button style={{ fontSize: 30 }} className="btn-clear" onClick={toggleTheme}>
+          <ul className="row navbar">
+            <li>
+              <NavLink exact to="/" activeStyle={activeStyle} className="nav-link">
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/battle" activeStyle={activeStyle} className="nav-link">
+                Battle
+              </NavLink>
+            </li>
+          </ul>
+          <button style={{ fontSize: 30, height: 50 }} className="btn-clear" onClick={toggleTheme}>
             {theme === 'light' ? '🌙' : '☀️'}
           </button>
         </nav>
