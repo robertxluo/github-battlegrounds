c07b3dab34060e1a738a1a86aadb6b3f2bb7bf05
robertxluo <robertxluo@gmail.com>
2019-12-19 23:30:35 -0600

    Change repositories to show 100

--- app/utils/api.js
+++ app/utils/api.js
@@ -57,7 +57,7 @@ export function battle(players) {
 
 export function fetchPopularRepos(language) {
   const endpoint = window.encodeURI(
-    `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+    `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories&per_page=100`
   );
 
   return fetch(endpoint)
